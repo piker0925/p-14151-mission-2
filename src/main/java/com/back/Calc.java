b5158b9a98d0c636b72dd02ce681d485db31bb73
JUNSEO OH <piker0925@gmail.com>
2026-04-22 16:51:16 +0900

    t28까지 통과

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -4,26 +4,68 @@ public class Calc {
     public static int run(String num) {
         num = num.trim();
 
-        int plus = num.lastIndexOf(" + ");
-        int minus = num.lastIndexOf(" - ");
-        int index = Math.max(plus, minus);
+        if (num.startsWith("(") && num.endsWith(")")) {
+            int depth = 0;
+            boolean isBracket = true;
+            for (int i = 0; i < num.length(); i++) {
+                char c = num.charAt(i);
+                if (c == '(') depth++;
+                else if (c == ')') depth--;
 
-        if (index != -1) {
-            String left = num.substring(0, index);
-            String right = num.substring(index + 3);
-            
-            if (num.charAt(index + 1) == '+') {
-                return run(left) + run(right);
-            } else {
-                return run(left) - run(right);
+                if (depth == 0 && i < num.length() - 1) {
+                    isBracket = false;
+                    break;
+                }
+            }
+            if (isBracket) {
+                return run(num.substring(1, num.length() - 1).trim());
             }
         }
 
-        int multi = num.lastIndexOf(" * ");
-        if (multi != -1) {
-            String left = num.substring(0, multi);
-            String right = num.substring(multi + 3);
+        int index = -1;
+        int depth = 0;
+        for (int i = num.length() - 1; i >= 0; i--) {
+            char c = num.charAt(i);
+            if (c == ')') depth++;
+            else if (c == '(') depth--;
+
+            if (depth == 0) {
+                if (num.startsWith(" + ", i) || num.startsWith(" - ", i)) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+
+        if (index != -1) {
+            String left = num.substring(0, index).trim();
+            String right = num.substring(index + 3).trim();
+            char op = num.charAt(index + 1);
+
+            if (op == '+') return run(left) + run(right);
+            else return run(left) - run(right);
+        }
+
+        depth = 0;
+        for (int i = num.length() - 1; i >= 0; i--) {
+            char c = num.charAt(i);
+            if (c == ')') depth++;
+            else if (c == '(') depth--;
+
+            if (depth == 0 && num.startsWith(" * ", i)) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1) {
+            String left = num.substring(0, index).trim();
+            String right = num.substring(index + 3).trim();
             return run(left) * run(right);
+        }
+
+        if (num.startsWith("-")) {
+            return -1 * run(num.substring(1).trim());
         }
 
         return Integer.parseInt(num);
